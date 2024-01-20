66ee65180b69d61ba632917c940cf0c79c6ce6c1
Zeph <zeph.levin@gmail.com>
2024-01-20 11:32:17 -0500

    Started payment calculator

--- docs/content/js/site.js
+++ docs/content/js/site.js
@@ -9,9 +9,27 @@ lutil = (function() {
         localStorage.setItem(key, val);
     }
 
+    function clearItems(items) {
+        if (Array.isArray(items)) {
+            items.forEach((x) => localStorage.removeItem(x));
+        }
+        else {
+            localStorage.removeItem(items);
+        }
+    }
+
     publicApi.consts = {
+        /* Index */
         MILEAGEKEY : "MILEAGEKEY",
-        STARTDATEKEY : "STARTDATEKEY"
+        STARTDATEKEY : "STARTDATEKEY",
+
+        /* Payments */ 
+        MSRP:                       "MSRPKEY",
+        NEGOTIATEDVEHICLEPRICE:     "NVPKEY",
+        DOWNPAYMENT:                "DPKEY",
+        LEASETERMS:                 "LEASETERMSKEY",
+        MONEYFACTOR:                "MONEYFACTOR",
+        RESIDUALVALUE:              "RESIDUALVALUEKEY"
     };
     publicApi.getItem = getItem;
     publicApi.setItem = setItem;

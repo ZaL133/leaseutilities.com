ed64afcb490842cac5076c6f3cf7ae80ad3790cd
Zeph <zeph.levin@gmail.com>
2024-02-08 16:29:18 -0500

    Added Annual Allowed & Overage

--- docs/content/js/site.js
+++ docs/content/js/site.js
@@ -1,9 +1,57 @@
+let consts = {
+        /* Index */
+        MILEAGEKEY : "MILEAGEKEY",
+        STARTDATEKEY : "STARTDATEKEY",
+        MILEAGEHISTORYKEY: "MILEAGEHISTORYKEY",
+        ANNUALMILESKEY: "ANNUALMILESKEY",
+
+        /* Payments */ 
+        MSRP:                       "MSRPKEY",
+        NEGOTIATEDVEHICLEPRICE:     "NVPKEY",
+        DOWNPAYMENT:                "DPKEY",
+        LEASETERMS:                 "LEASETERMSKEY",
+        MONEYFACTOR:                "MONEYFACTOR",
+        RESIDUALVALUE:              "RESIDUALVALUEKEY"
+    };
+
 lutil = (function() {
     let publicApi = {};
 
     function getItem(key) {
         return localStorage.getItem(key);
     }
+
+    /*
+        Save each mileage log, only once per day - keep the last entry
+        mileageLogInfo {
+            logDate : DateTime,
+            miles: int
+        }
+     */
+    function logMileage(mileageInfo) {
+        // make sure the mileageInfo object is valid
+        if (
+            mileageInfo.hasOwnProperty("logDate")
+            && moment(mileageInfo["logDate"]) != "Invalid Date"
+            && mileageInfo.hasOwnProperty("miles")
+            && parseFloat(mileageInfo["miles"]) != NaN
+        ) {
+            let log = {};
+            try {
+                let log     = JSON.parse(getItem(consts.MILEAGEHISTORYKEY));
+            } catch {}
+
+            let logDate = mileageInfo["logDate"];
+            let miles   = mileageInfo["miles"];
+
+            log[logDate] = miles;
+
+            var logString = JSON.stringify(log);
+
+            setItem(consts.MILEAGEHISTORYKEY, logString);
+        }
+    }
+
 
     function setItem(key, val) {
         localStorage.setItem(key, val);
@@ -18,21 +66,10 @@ lutil = (function() {
         }
     }
 
-    publicApi.consts = {
-        /* Index */
-        MILEAGEKEY : "MILEAGEKEY",
-        STARTDATEKEY : "STARTDATEKEY",
-
-        /* Payments */ 
-        MSRP:                       "MSRPKEY",
-        NEGOTIATEDVEHICLEPRICE:     "NVPKEY",
-        DOWNPAYMENT:                "DPKEY",
-        LEASETERMS:                 "LEASETERMSKEY",
-        MONEYFACTOR:                "MONEYFACTOR",
-        RESIDUALVALUE:              "RESIDUALVALUEKEY"
-    };
-    publicApi.getItem = getItem;
-    publicApi.setItem = setItem;
+    publicApi.consts = consts;
+    publicApi.getItem       = getItem;
+    publicApi.logMileage    = logMileage;
+    publicApi.setItem       = setItem;
 
     return publicApi;
 })();

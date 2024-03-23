c52533b84a9227e2524c358330fc059c0a448ab3
Zeph <zeph.levin@gmail.com>
2024-03-23 11:14:48 -0400

    Fixed bug where first record is not calculating average

--- docs/content/js/pages/mileage-history.js
+++ docs/content/js/pages/mileage-history.js
@@ -11,11 +11,18 @@ $(function() {
         var dailyAvgBetween = 0;
 
         if (i > 0) {
-          var lastEntry     = sortedLog[i - 1];
+          var lastEntry     = sortedLog[i - 1]; 
           var endDate       = moment(entry.logDate);
           var startDate     = moment(lastEntry.logDate);
           var daysBetween   = endDate.diff(startDate, "days");
           var milesBetween  = entry.miles - lastEntry.miles;
+          dailyAvgBetween   = milesBetween / daysBetween;
+        } else {
+          var endDate       = moment(entry.logDate);
+          var startDateString = lutil.getItem(consts.STARTDATEKEY);
+          var startDate     = moment(startDateString);
+          var daysBetween   = endDate.diff(startDate, "days");
+          var milesBetween  = entry.miles;
           dailyAvgBetween   = milesBetween / daysBetween;
         }
         $(".table tbody").append(`<tr><td>${entry.logDate}</td><td>${entry.miles}</td><td>${dailyAvgBetween.toFixed(2)}</td></tr>`);
